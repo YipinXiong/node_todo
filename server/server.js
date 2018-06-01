5d917fb15ec3933d08e20ba5c92e7dff98100c27
Yipin <xiong1pin@gmail.com>
2018-06-01 21:38:59 +1000

    add test get

--- server/server.js
+++ server/server.js
@@ -22,12 +22,11 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-	Todo.find().then((todos)=> {
-		//pass it as an object for it makes possible to add another info
-		res.send({todos});
-	}, (e) =>{
-		res.status(400).send(e);
-	})
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
